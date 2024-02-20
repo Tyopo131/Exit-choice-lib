a4b20f91ed9ba3702183bc90532bb308d63d36ae
T-yopo <samecob62@gmail.com>
2024-02-20 14:51:37 +0000

    Added more friendly names

--- StandaloneExitChoice/StandaloneExitChoice.h
+++ StandaloneExitChoice/StandaloneExitChoice.h
@@ -3,6 +3,7 @@ namespace stdln {
 	_declspec(dllexport) void exitchoice();
 	_declspec(dllexport) extern char key1;
 	_declspec(dllexport) extern char key2;
-	_declspec(dllexport)extern char okey1;
-	_declspec(dllexport)extern char okey2;
+	_declspec(dllexport) extern char okey1;
+	_declspec(dllexport) extern char okey2;
+	_declspec(dllexport) extern char restartKey1, restartKey2, exitKey1, exitKey2;
 }
--- StandaloneExitChoice/dllmain.cpp
+++ StandaloneExitChoice/dllmain.cpp
@@ -3,19 +3,17 @@
 #include <conio.h>
 using namespace std;
 namespace stdln {
+	char restartKey1, restartKey2, exitKey1, exitKey2;
 	char key1, key2, okey1, okey2;
 	void exitchoice() {
 		char keyHit;
 		while (true) {
 			if (_kbhit()) {
 				keyHit = _getch();
-				if (keyHit == key1 || keyHit == key2) {
+				if (keyHit == key1 || keyHit == key2 || keyHit == restartKey1 || keyHit == restartKey2) {
 					callmain();
 				}
-				else if (keyHit == okey1 || keyHit == okey2) return;
-				
-				
-
+				else if (keyHit == okey1 || keyHit == okey2 || keyHit == exitKey1 || keyHit == exitKey2) return;
 			}
 		}
 	}
